834378d2c420d3a85e46441f875ae76706670a7c
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2015-11-13 10:55:23 -1000

    rename cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name              "keepalived"
+name              "chef-keepalived"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures keepalived"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.2.0"
+version           "1.3.0"
 supports          "ubuntu"
 
 recipe "keepalived", "Installs and configures keepalived"
